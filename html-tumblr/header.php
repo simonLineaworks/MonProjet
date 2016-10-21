67189c21574b782ab89eeafc8ea5d8192f583d74
simonLineaworks <simon@lineaworks.jp>
2016-10-21 18:08:40 +0900

    delicious post

---
+++ html-tumblr/header.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<?php endif; ?>
+
+	<script src="https://use.typekit.net/bmj0uty.js"></script>
+	<script>try{Typekit.load({ async: true });}catch(e){}</script>
+	<link rel="stylesheet" href="../wp-content/themes/Perso simon-harajuku/font-awesome/css/font-awesome.min.css">
+
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+	<link rel="stylesheet" type="text/css" href="../wp-content/themes/Perso simon-harajuku/js/slick/slick.css"/>
+	<link rel="stylesheet" type="text/css" href="../wp-content/themes/Perso simon-harajuku/js/slick/slick-theme.css"/>	
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<!-- Latest compiled JavaScript -->
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+	<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+	<script type="text/javascript" src="../wp-content/themes/Perso simon-harajuku/js/slick/slick.min.js"></script>
+	<script src="../wp-content/themes/Perso simon-harajuku/js/jquery.tile.min.js" type="text/javascript" charset="utf-8"></script>
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+<header >
+
+	<nav class="navbar navbar-inverse navbar-fixed-top" id="nav-top" >
+	<!-- You can change css position:absolute -> position:fixed --> 
+
+ 		<div class="container" id="" style="">				
+		
+
+  			<ul class="nav navbar-nav" id="nav-left" style="">
+
+  				<li class="menu-pc"><span class="glyphicon glyphicon-menu-hamburger" id="hamburger" style=""></span></li>
+
+      			<li class="dropdown" style="">
+	    			<a class="dropdown-toggle" data-toggle="dropdown" href="#" ><span class="glyphicon glyphicon-menu-hamburger"></span></a>
+	    			
+	    			<ul class="dropdown-menu">
+	      				<li><a href="#">KITCHEN</a></li>
+						<li><a href="#">SHOP</a></li>
+						<li><a href="#">COOKING</a></li>
+						<li><a href="#">ANTENNA SHOP</a></li>
+	    			</ul>
+	  			</li>
+	  
+			    <li class="menu-pc"><a href="#">KITCHEN</a></li>
+			    <li class="menu-pc"><a href="#">SHOP</a></li>
+			    <li class="menu-pc"><a href="#">COOKING</a></li> 
+			    <li class="menu-pc"><a href="#">ANTENNA SHOP</a></li> 
+    		</ul>
+    
+    	</div>
+   
+    	<div class="container" id="social-top">				
+			<div class="navbar-down">
+				<ul>
+					<li><a href="#" ><img src="../wp-content/themes/Perso simon-harajuku/images/facebook.png" style="" alt="" ></a></li>
+					<li><a href="#"><img src="../wp-content/themes/Perso simon-harajuku/images/instagram.png" style="" alt=""></a></li>
+					<li><a href="#"><img src="../wp-content/themes/Perso simon-harajuku/images/twitter.png" style="" alt=""></a></li>
+					<li><a href="#"><img src="../wp-content/themes/Perso simon-harajuku/images/G+.png" style="" alt=""></a></li>	        
+				</ul>			
+			</div>
+		</div>
+	</nav>
+	
+</header><!-- .site-header -->
+
+<div>
---
+++ html-tumblr/index.php
@@ -0,0 +1,298 @@
+<?php get_header(); ?>
+
+	<div class="mv" style="">
+
+		<div class="head-slider">
+
+	  		<div><img src="../wp-content/themes/Perso simon-harajuku/images/100666-188.png" alt="" class="img-responsive background-image" style=""></div>
+	  		
+	  		<div><img src="../wp-content/themes/Perso simon-harajuku/images/100666-209.png" alt="" class="background-image" style=""></div> 
+
+		</div>
+
+		<div class="head-image-sm"><img src="../wp-content/themes/Perso simon-harajuku/images/100666-188-sm.png" alt="" class="img-responsive" style=""></div> 
+
+		<h2 class="ttl"><img src="../wp-content/themes/Perso simon-harajuku/images/harajuku.png" alt="" class="name" style=""></h2>
+
+	</div>
+
+
+	<div class="container">
+ 
+		<div class="modal fade" id="myModal" role="dialog" style="">
+		    <div class="modal-dialog modal-lg">      
+		      	<div class="modal-content">  
+		        	<div class="modal-body">
+
+		          		<button type="button" class="close" data-dismiss="modal">&times;</button>
+		         		
+		         		<div class="row">
+
+						    <div class="col-sm-2 col-xs-2" style=""><img src="../wp-content/themes/Perso simon-harajuku/images/modal-pink.png" alt="" class="img-responsive"></div>
+
+						    <div class="col-sm-10 col-xs-10" style="">
+						    	<h1 style="">About Tea Stand 7</h1>
+						    </div>
+				  		</div>
+
+				  		<img src="../wp-content/themes/Perso simon-harajuku/images/stand.png" class="modal-stand" alt="" style="">
+		        	</div>  
+		      	</div> 
+		    </div>
+		</div> 
+	</div>
+
+
+	<div class="tea-stand-circle" style="">	
+		<a data-toggle="modal" data-target="#myModal">
+			<img src="../wp-content/themes/Perso simon-harajuku/images/IMG_4357.png" alt="">
+		</a>
+	</div>
+
+	<div class="tea-stand" style="">	
+		<a data-toggle="modal" data-target="#myModal">
+			<img src="../wp-content/themes/Perso simon-harajuku/images/stand.png" alt="">
+		</a>
+	</div>
+
+
+	<div class="deliver-farm-fresh" >
+		<img src="../wp-content/themes/Perso simon-harajuku/images/FarmFresh.png" alt="" class="img-responsive" style=" ">
+		<h3>産地直送品をお届けします</h3>
+		<p>都会（はらじゅく）と地方（畑）を結ぶ発信基地。<br>全国各地の素敵な物や事を広めるアンテナの役割をする会社です。</p>
+		<button type="button" class="btn btn-default">View More Information</button>
+	</div>
+
+
+	<div class="service">
+		<div class="description" >
+			<h1>Service</h1>
+			<p>はらじゅく畑のサービス</p>
+		</div>
+
+		<img src="../wp-content/themes/Perso simon-harajuku/images/100666-102.png" >	
+	</div>
+
+
+	<div class="kitchen" >
+
+		<img src="../wp-content/themes/Perso simon-harajuku/images/100666-228.png" alt="" class="img-responsive up">
+		<img src="../wp-content/themes/Perso simon-harajuku/images/100666-149.png" alt="" class="img-responsive down">
+		
+		<div class="description" >
+
+			<h1>Kitchen</h1>
+			<h3>はらじゅく畑のKITCHEN</h3>
+			<p>全国各地の美味しい食材をきみママが厳選し、普段食べられるようでなかなか食べられない贅沢な家庭料理のお店。<br>家庭料理研究科のきみママが作る家庭料理はとってもシンプル。ショップ内で販売している食材を使いお客様の前で作ります。完全予約制なので一人一人のお腹の具合も聞きながら提供します。ちょっとした作り方も見たり聞いたりできるので帰ってからも家庭で再現できる事がはらじゅく畑キッチンの売りです。</p>
+			<button type="button" class="btn btn-default">View More Information</button>
+
+		</div>
+	</div>
+
+
+	<div class="cooking" >
+		<div class="description" >
+
+			<h1>Cooking</h1>
+			<h3>はらじゅく畑の料理教室</h3>
+			<p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+			<button type="button" class="btn btn-default">View More Information</button>
+		</div>
+
+		<img src="../wp-content/themes/Perso simon-harajuku/images/100666-181.png" alt="" class="img-responsive" style="">
+
+	</div>
+
+
+	<div class="shop" >
+		<div class="description" style="" >
+
+			<h1>Shop</h1>
+			<h3>はらじゅくで買える、伝えたい、産地の直送品。</h3>
+			<p>全国各地の美味しい食材をきみママが厳選し、普段食べられるようでなかなか食べられない贅沢な家庭料理のお店。<br>家庭料理研究科のきみママが作る家庭料理はとってもシンプル。ショップ内で販売している食材を使いお客様の前で作ります。完全予約制なので一人一人のお腹の具合も聞きながら提供します。ちょっとした作り方も見たり聞いたりできるので帰ってからも家庭で再現できる事がはらじゅく畑キッチンの売りです。</p>
+			<button type="button" class="btn btn-default">View More Information</button>
+		</div>
+
+		<img src="../wp-content/themes/Perso simon-harajuku/images/100666-118.png" alt="" class="img-responsive" style="">
+
+	</div>
+
+
+	<div class="antenna-shop">
+		<div class="description" >
+
+			<h1>Antenna Shop</h1>
+			<h3>はらじゅく畑の料理教室</h3>
+			<p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+			<button type="button" class="btn btn-default">View More Information</button>
+		</div>
+
+		<img src="../wp-content/themes/Perso simon-harajuku/images/100666-97.png" alt="" style="" class="img-responsive">
+
+	</div>
+		
+
+	<div class="row" id="news">
+		<div class="col-md-6 col-sm-12 col-xs-12">
+	  		<h3>NEWS RELEASE</h3>
+	  		<ul id="list-posts" style="">
+				<div class="row" id="news-post" style="">
+					<?php 
+						$args = array(
+					        'post_type' => 'news_posts',
+					        'posts_per_page' => 4
+					    );
+
+	    				$the_query = new WP_Query( $args ); 
+						if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); 
+					?>
+
+			    	<div class="col-sm-3 col-xs-3" style="">
+			    		<!-- ** Display the post title ** -->	
+					   <li><a style="" href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></li>
+
+						<!--	** Or you can display the date of the post	**  
+			 			<li><a style="" href="<?php the_permalink() ?>" rel="bookmark"><?php the_date(); ?></a></li>   -->
+
+					</div>
+
+					<div class="col-sm-9 col-xs-9" style="">
+					   <li><a style="" href="<?php the_permalink() ?>" rel="bookmark"><?php the_content() ?></a></li>
+					</div>
+
+
+		<?php endwhile; else : ?>
+		<?php endif;
+		wp_reset_postdata(); ?>
+
+
+				</div>
+			</ul>
+	  	</div>
+
+
+  		<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12" id="calendar" style="" >
+
+			<h3>CALENDAR</h3>
+
+			<div class="new-calendar">
+
+		  		<?php
+					$args= array(
+					    'next_text' => 'NEXT >',
+					    'prev_text' => '< PREV',
+					    'post_count' => false,
+					    'month_view' => true
+					);
+					archive_calendar($args);
+				?>
+
+			</div>	
+  		</div> 
+	</div>
+
+
+	<div class="company-information">
+		<div class="description" >
+
+			<h1>Company Information</h1>
+			<p>はらじゅく畑のサービス</p>
+			<button type="button" class="btn btn-default">View More Information</button>
+
+		</div>
+	
+		<img src="../wp-content/themes/Perso simon-harajuku/images/100666-164.png"  style= "">	
+	
+	</div>
+
+
+	<div class="delicious-posts" >
+ 		<div class="container">
+			<div class="delicious-title">
+				<img src="../wp-content/themes/Perso simon-harajuku/images/DeliciousPosts.png" class="" alt="" style="">
+			</div>
+
+			<div class="test">
+	
+	<?php 
+					$args = array(
+		        		'post_type' => array('instagram_posts', 'post'),
+		    		 );
+
+		    		 $testPosts = new WP_Query( $args ); ?>
+
+					 
+
+	<?php 			if ( $testPosts->have_posts() ) : while ( $testPosts->have_posts() ) : $testPosts->the_post();?>
+
+
+				<div class="sns-posts col-lg-3 col-md-3 col-sm-4 col-xs-6">
+					<ul class="post-box">
+						<li><a style="" href="" rel="bookmark"><?php the_post_thumbnail(); ?></a></li>
+						
+						<li>
+							<div class="date-box">
+								<div class="col-sm-6 col-xs-6" id="date-tumblr" style="">
+									<p style="float: right;margin-bottom: 0;"><?php echo get_the_date();?></p>
+								</div> 
+
+								<div class="col-sm-6 col-xs-6" style=" padding: 0px 20px 0px 10px;">
+										<?php the_category(); ?>
+								</div>
+							</div>
+						</li>	
+
+						
+						<li class="title-box">	
+							<p><a style="" href="" rel="bookmark"><?php the_title(); ?></a></p>
+						</li>
+
+						<li>
+<?php
+	$content = strip_tags($post->post_content);
+	$ptn = '/Source: Tumblr$/';
+	$content = preg_replace($ptn, '', $content);
+	if($content != ''):
+?>
+							<div class="content-box">
+								<a style="" href="" rel="bookmark"><?php echo $content; ?></a>
+							</div>
+<?php
+	endif;
+?>
+						</li>
+
+					</ul>
+
+				</div>	
+
+
+
+
+				<?php endwhile; else : ?>
+				<?php endif;
+
+				wp_reset_postdata(); ?>
+
+				<div class="sns-posts col-lg-3 col-md-3 col-sm-4 col-xs-6" style="" id="delicious-image">
+					<div class="post-box"><img src="../wp-content/themes/Perso simon-harajuku/images/IMG_4357-social.png" alt="" style=""></div>
+				</div>
+<script>
+$(window).on('load', function(){
+	$('.post-box').tile(4);
+})
+</script>
+			</div>
+
+
+
+			</div>	
+		</div>		
+	</div>
+
+
+<script src="<?php echo plugins_url('archives-calendar-widget/admin/js/jquery.arcw-init.js'); ?>" type="text/javascript" charset="utf-8"></script>
+
+<script >$('.head-slider').slick();</script>
+
+<?php get_footer(); ?>
